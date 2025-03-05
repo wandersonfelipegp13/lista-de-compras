d0c12558d2e977534c95c6576b5d7a24718846cf
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2025-03-05 03:53:13 -0300

    Versão 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "br.com.wandersonfelipe.listadecompras"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
